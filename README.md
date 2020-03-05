48d0cee1a7d1a5904a749d74f3502d98e9e7954a
Luca J <34938395+luke-jj@users.noreply.github.com>
2020-03-05 15:20:32 +0100

    Fix middleware import naming conflict.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Go Weather Service REST API
+
+
+
+
+
+## Dev Notes
+### Using Reflex for hot reloading
+
+     reflex -r '\.go$' -s -- sh -c "go run *.go"
--- cmd/weatherd/startup/middleware.go
+++ cmd/weatherd/startup/middleware.go
@@ -2,19 +2,19 @@ package startup
 
 import (
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	chiMiddleware "github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
-	mw "github.com/luke-jj/go-weather-api/cmd/weatherd/middleware"
+	"github.com/luke-jj/go-weather-api/cmd/weatherd/middleware"
 	"github.com/luke-jj/go-weather-api/internal/models"
 )
 
 func Middleware(config *models.Config, router *chi.Mux) {
 	router.Use(
-		mw.GetCORSHandler(),
+		middleware.GetCORSHandler(),
 		render.SetContentType(render.ContentTypeJSON),
-		middleware.Logger,
-		middleware.Compress(5),
-		middleware.RedirectSlashes,
-		middleware.Recoverer,
+		chiMiddleware.Logger,
+		chiMiddleware.Compress(5),
+		chiMiddleware.RedirectSlashes,
+		chiMiddleware.Recoverer,
 	)
 }
